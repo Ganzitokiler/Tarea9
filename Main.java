9b7472debe69d6e2eb908bf12a68c2af2c6e9275
Ganzitokiler <126096942+Ganzitokiler@users.noreply.github.com>
2023-04-30 14:22:51 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+import iu.Principal;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
